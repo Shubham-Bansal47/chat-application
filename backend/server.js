028332908683fe3195798039ead24a773ac0a449
shubham <shubhambansal096@gmail.com>
2024-04-20 18:08:30 +0530

    Implemented logic for conversations backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 const app = express();
 
@@ -8,8 +10,10 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use("/api/auth", authRoutes);
+app.use(cookieParser());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 // app.get("/", (req, res) => {
 //   res.send("hello world||");
 // });
